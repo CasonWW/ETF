63aab5be46e5357d792ec654ff01df2915c9a9c1
Cason <Cason_w@outlook.com>
2020-10-16 22:57:13 +0800

    update push func

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import logging
 import schedule
 from push import push as Push
 from datetime import datetime
+import config
+import os
 
 # init logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M',
                     handlers=[logging.FileHandler('error.log', 'w', 'utf-8'), ])
-ark_c = 0
 
 
 def cal_change(fund, etf, last_updated_date, date):
@@ -54,10 +55,10 @@ def main(fund, url, agency, last_updated_date, ark_u):
         dataframe_to_mongo(etf_df)
         update_date(fund, date)
         print(fund, "|", date, "|", agency)
-        if agency in 'ARK':
-            return ark_u + 1
-        else:
-            return ark_u
+    if (agency in 'ARK') and (etf_df is not None):
+        return ark_u + 1
+    else:
+        return ark_u
 
 
 def start():
@@ -75,7 +76,7 @@ def start():
             print(fund, "|", last_updated_date, "|", agency)
 
             # if last_updated_date.weekday() != 4:
-            # if 'Invesco' in ETF['agency']:
+            # if 'ARK' in ETF['agency']:
             ark_u = main(fund, url, agency, last_updated_date, ark_u)
             time.sleep(5)
             # 10 16 20
@@ -84,9 +85,7 @@ def start():
 
         except Exception as Argument:
             logging.error('ETF', ETF['fund'], ':\nThis is the Argument:', Argument)
-
-    if ark_u is not 0:
-        print(ark_u)
+    if (ark_u is not 0) and (ark_u is not None):
         Push('d')
     elif ark_u is not 0 and datetime.today().weekday() is 5:
         Push('w')
@@ -94,29 +93,60 @@ def start():
     print('Done! ')
 
 
+def backup():
+    backup_path = config.Backup_Path + time.strftime("%Y-%m-%d-%H:%M:%S")
+    host = config.DB_Host
+    port = config.DB_Port
+    username = config.DB_User
+    password = config.DB_Passwd
+
+    command = "mongodump"
+    if host != 'NA':
+        command += " --host " + host
+    if port != 'NA':
+        command += " --port " + port
+    if username != 'NA':
+        command += " --username " + username
+    if password != 'NA':
+        command += " --password " + password
+
+    command += " -d ETF --out " + backup_path
+
+    os.system(command)
+
+    print("mongo backup done")
+
+    os.system(f'zip -q -r {backup_path}.zip ' + backup_path)
+
+
 if __name__ == '__main__':
-    # schedule.every().monday.at("12:00").do(start)
-    # schedule.every().monday.at("20:00").do(start)
-    # schedule.every().tuesday.at("09:00").do(start)
-    # schedule.every().tuesday.at("12:00").do(start)
-    # schedule.every().tuesday.at("18:00").do(start)
-    # schedule.every().tuesday.at("21:20").do(start)
-    # schedule.every().wednesday.at("09:00").do(start)
-    # schedule.every().wednesday.at("12:00").do(start)
-    # schedule.every().wednesday.at("18:00").do(start)
-    # schedule.every().wednesday.at("21:20").do(start)
-    # schedule.every().thursday.at("09:00").do(start)
-    # schedule.every().thursday.at("12:00").do(start)
-    # schedule.every().thursday.at("18:00").do(start)
-    # schedule.every().thursday.at("21:20").do(start)
-    # schedule.every().friday.at("09:00").do(start)
-    # schedule.every().friday.at("12:00").do(start)
-    # schedule.every().friday.at("18:00").do(start)
-    # schedule.every().friday.at("21:20").do(start)
-    # schedule.every().saturday.at("09:00").do(start)
-    # schedule.every().saturday.at("18:00").do(start)
-    #
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-    start()
+    print('Start Programme!')
+    schedule.every().monday.at("12:00").do(start)
+    schedule.every().monday.at("22:00").do(start)
+    schedule.every().tuesday.at("00:00").do(backup)
+    schedule.every().tuesday.at("09:00").do(start)
+    schedule.every().tuesday.at("12:00").do(start)
+    schedule.every().tuesday.at("18:00").do(start)
+    schedule.every().tuesday.at("22:00").do(start)
+    schedule.every().wednesday.at("00:00").do(backup)
+    schedule.every().wednesday.at("09:00").do(start)
+    schedule.every().wednesday.at("12:00").do(start)
+    schedule.every().wednesday.at("18:00").do(start)
+    schedule.every().wednesday.at("22:00").do(start)
+    schedule.every().thursday.at("00:00").do(backup)
+    schedule.every().thursday.at("09:00").do(start)
+    schedule.every().thursday.at("12:00").do(start)
+    schedule.every().thursday.at("18:00").do(start)
+    schedule.every().thursday.at("22:00").do(start)
+    schedule.every().friday.at("00:00").do(backup)
+    schedule.every().friday.at("09:00").do(start)
+    schedule.every().friday.at("12:00").do(start)
+    schedule.every().friday.at("18:00").do(start)
+    schedule.every().friday.at("22:00").do(start)
+    schedule.every().saturday.at("09:00").do(start)
+    schedule.every().saturday.at("18:00").do(start)
+    schedule.every().sunday.at("00:00").do(backup)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
--- push.py
+++ push.py
@@ -2,14 +2,14 @@ from api import f, dc
 from database import get_etf_list_by_query, get_etf_data
 import pandas as pd
 import os.path
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
-
-ark_etf_list = list(get_etf_list_by_query({'agency': 'ARK'}))
 
 
 def daily():
-    path = 'daily_change.csv'
+    ark_etf_list = list(get_etf_list_by_query({'agency': 'ARK'}))
+
+    path = 'ark_daily_change.csv'
     if os.path.isfile(path):
         os.remove(path)
 
@@ -27,16 +27,19 @@ def daily():
     df = df.drop(columns=['_id']).sort_values(by=['ticker', 'date'])
     df.to_csv(path, index=False)
     dc(path)
+
     df['shares'] = df['shares'].astype(np.int64).astype(str)
     df['date'] = df['date'][0].strftime('%Y-%m-%d')
     df = df.reset_index(drop='index')
     md = df.to_markdown()
-    f(md)
+    # f(md)
+
     print('pushed daily change')
 
 
 def weekly():
-    path = 'weekly_change.csv'
+    ark_etf_list = list(get_etf_list_by_query({'agency': 'ARK'}))
+    path = 'ark_weekly_change.csv'
     if os.path.isfile(path):
         os.remove(path)
 
@@ -45,8 +48,8 @@ def weekly():
     for ark_etf in ark_etf_list:
         ark_data_list = list(
             get_etf_data(
-                {'fund': ark_etf['fund'], 'date': {'$gte': datetime.strptime('20201005', '%Y%m%d'),
-                                                   '$lte': datetime.strptime('20201009', '%Y%m%d')},
+                {'fund': ark_etf['fund'], 'date': {'$gte': ark_etf['last_updated_date'] + timedelta(days=-5),
+                                                   '$lte': ark_etf['last_updated_date']},
                  'change': {'$ne': 0}}))
         if len(ark_data_list) is not 0:
             etf_data = etf_data + ark_data_list
